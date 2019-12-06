4cca09e5f3ddaf725e63e3b46f2bed83a5c8f918
kenblikylee <kenblikylee@126.com>
2019-12-06 10:32:05 +0800

    avatar support bgcolor

--- pitu/avatar.py
+++ pitu/avatar.py
@@ -3,9 +3,10 @@ from .utils import centerratio, expandratio
 
 def avatar(photo, params, show=False):
     _photo = Photo(photo)
-    if _parse(params)['mode'] == 'ex':
+    _params = _parse(params)
+    if _params['mode'] == 'ex':
         w, h, x, y = expandratio(_photo.size())
-        _photo = EmptyPhoto(width=w, height=h).paste(_photo, x, y)
+        _photo = EmptyPhoto(width=w, height=h, bgcolor=_params['bgcolor']).paste(_photo, x, y)
     else:
         _w, _h = _size = _photo.size()
         _center = int(_w/2), int(_h/2)
@@ -16,6 +17,7 @@ def avatar(photo, params, show=False):
 
 def _parse(params):
     if params is None:
-        return dict(mode = 'ce')
+        return dict(mode = 'ce', bgcolor = 'white')
     _len = len(params)
-    return dict(mode = (str(params[0]) if _len > 0 else None))
+    return dict(mode = (str(params[0]) if _len > 0 else None),
+                bgcolor = (str(params[1]) if _len > 1 else 'white'))
